cf7a7973800aae480b4759d13ce243a76cf90311
Etienne Januel <etienne.januel@laplateforme.io>
2021-03-03 23:57:41 +0100

    init

---
+++ api/app/Config/Routes.php
@@ -0,0 +1,39 @@
+<?php namespace Config;
+
+// Create a new instance of our RouteCollection class.
+$routes = Services::routes();
+
+// Load the system's routing file first, so that the app and ENVIRONMENT
+// can override as needed.
+if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
+{
+	require SYSTEMPATH . 'Config/Routes.php';
+}
+
+/**
+ * --------------------------------------------------------------------
+ * Router Setup
+ * --------------------------------------------------------------------
+ */
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+$routes->setAutoRoute(false);
+
+/**
+ * --------------------------------------------------------------------
+ * Route Definitions
+ * --------------------------------------------------------------------
+ */
+
+/**
+ * --------------------------------------------------------------------
+ * Additional Routing
+ * --------------------------------------------------------------------
+ */
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
+{
+	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+}
---
+++ mobile/components/Home/Home.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default Home = () => {
+  return (
+    <View style={style.bigContainer}>
+      <View style={style.main}>
+        <NavigationContainer>
+          <DrawerScreen />
+        </NavigationContainer>
+      </View>
+    </View>
+  );
+};
+
+const HomeFeed = () => {
+  return <View />;
+};
+
+const Drawer = createDrawerNavigator();
+const DrawerScreen = () => (
+  <Drawer.Navigator initialRouteName="HomeFeed">
+    <Drawer.Screen name="HomeFeed" component={HomeFeed} />
+  </Drawer.Navigator>
+);
+
+const style = StyleSheet.create({
+  bigContainer: {
+    flex: 1,
+  },
+  main: {
+    flex: 4,
+    width: '100%',
+    justifyContent: 'flex-start',
+    borderBottomRightRadius: 50,
+    borderBottomLeftRadius: 50,
+    backgroundColor: '#E7EEF5',
+  },
+});
